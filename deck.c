4a221df9cb12dbffb3475e8553db31693abca6b9
matheuscamarques <37029621+matheuscamarques@users.noreply.github.com>
2021-04-04 19:40:08 -0300

    UHUUU
    UHUUU

--- deck.c
+++ deck.c
@@ -7,12 +7,19 @@ Deck* createDeck () {
 /*Função que retorna a coordenada armazenada na cabeça da lista encadeada (assuma que o endereço do Deck *d aponta para a cabeça)!*/
 Point getFront (Deck *d) {
    /*Terminar ...*/
-'
+   return d->p;
 }
 
 /*Função que retorna a coordenada armazenada na cauda da lista encadeada (assuma que o endereço do Deck *d aponta para a cabeça)!*/
 Point getRear (Deck *d) {
    /*Terminar ...*/
+    Deck *aux = d;
+    while (aux->next != NULL)
+    {
+        aux = aux->prev;
+    }
+
+    return aux->p;
 }
 
 /*Função para inserir na cabeça da lista encadeada (assuma que o endereço do Deck *d aponta para a cabeça)!*/
@@ -20,13 +27,34 @@ Deck* insertFront (Deck *d, Point p, char field[][SIZE]) {
    /*Terminar ...*/
    field[p.x][p.y] = '*'; /*Ponto inserido faz parte do corpo da Snake!*/
    /*Terminar ...*/
+   Deck *novo = (Deck*)malloc(sizeof(Deck));
+    novo->p = p;
+    novo->proximo = no;
+    novo->anterior = NULL;
+  
+    if(d != NULL){
+      d->prev = novo;
+    }
+  
+    return novo;
 }
 
 /*Função para inserir na cauda da lista encadeada (assuma que o endereço do Deck *d aponta para a cabeça)!*/
 Deck* insertRear (Deck *d, Point p, char field[][SIZE]) {
    /*Terminar ...*/
-   field[p.x][p.y] = '*';  /*Ponto inserido faz parte do corpo da Snake!*/
-   /*Terminar ...*/
+  field[p.x][p.y] = '*';  /*Ponto inserido faz parte do corpo da Snake!*/
+  Node *aux = d;
+  while(aux->next != NULL){
+      aux = aux->next;  
+  }
+  Node *novo = (Deck*)malloc(sizeof(Deck));
+  novo->next = NULL;
+  novo->prev = aux;
+  novo->p = p;
+  aux->next = novo;
+  
+  return novo;
+  /*Terminar ...*/
 }
 
 /*Função para remover a cabeça da lista encadeada (assuma que o endereço do Deck *d aponta para a cabeça)!*/
@@ -34,6 +62,11 @@ Deck* deleteFront (Deck *d, char field[][SIZE]) {
    /*Terminar ...*/
    //field[x][y] = ' '; /*Ponto eliminado agora é marcado como vazio!*/
    /*Terminar ...*/
+  Node *aux = d->next;
+  field[d->p->x][d->p->y] = ' ';
+  free(d);
+  aux->prev = NULL;
+  return aux;
 }
 
 /*Função para remover a cauda da lista encadeada (assuma que o endereço do Deck *d aponta para a cabeça)!*/
@@ -41,6 +74,16 @@ Deck* deleteRear (Deck *d, char field[][SIZE]) {
    /*Terminar ...*/
    //field[x][y] = ' '; /*Ponto eliminado agora é marcado como vazio!*/
    /*Terminar ...*/
+  Deck aux * = d;
+  while(aux->next != NULL){
+    aux = aux->next;
+  }
+  
+  field[aux->p->x][aux->p->y] = ' ';
+  aux->next->prev = NULL;
+  free(aux);
+  
+  return no;
 }
 
 
